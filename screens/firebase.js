bd8feda5fe8b6f3657bf50456fdd34886d404f3b
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2021-03-21 13:54:46 +0000

    initalize project

---
+++ screens/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCqDlYZkkXtMois8TlGVT2uMHTeCIbNfKc',
+  authDomain: 'dev-mingle-build.firebaseapp.com',
+  projectId: 'dev-mingle-build',
+  storageBucket: 'dev-mingle-build.appspot.com',
+  messagingSenderId: '486273301055',
+  appId: '1:486273301055:web:f2d277343d126ae8c1168a',
+};
