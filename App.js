54472000748aa4f6b59d6c588a1ff62093f2c8e5
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2021-03-22 08:57:10 +0000

    ADD:login screen

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+const Stack = createStackNavigator();
+
+const globalScreenOption = {
+  headerStyle: { backgroundColor: 'red' },
+  headerTitleStyle: { color: 'white' },
+  headerTintColor: 'white',
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets Build</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOption}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/LoginScreen.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Button, Input, Image } from 'react-native-elements';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { KeyboardAvoidingView } from 'react-native';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const signIn = () => {};
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar styles="light" />
+      <Image
+        source={{
+          uri: '../assets/Number 2 PNG images free download_PNG14925.png',
+        }}
+        style={{ width: 200, height: 200, marginBottom: 50 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Sign Up" />
+      <View style={{ height: 200 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  inputContainer: { width: 300 },
+  button: { width: 200, marginTop: 10 },
+});
